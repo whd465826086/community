ba9e00ab72e383f76e3ef605f111b74f9b060378
whd465826086 <465826086@qq.com>
2020-01-12 19:52:09 +0800

    cookie

--- src/main/java/com/example/community_test/controller/IndexController.java
+++ src/main/java/com/example/community_test/controller/IndexController.java
@@ -1,9 +1,15 @@
 package com.example.community_test.controller;
 
+import com.example.community_test.mapper.UserMapper;
+import com.example.community_test.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by 王海东1997 on 2019/12/19.
@@ -11,8 +17,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    request.getSession().setAttribute("user", user);
+                    break;
+                }
+            }
+        }
         return "index";
     }
 }
